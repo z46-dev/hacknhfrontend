6ee35d03288ba1ddb4d10451b714e4011fa1a89f
Dan McCarthy <dmccrthy@users.noreply.github.com>
2024-11-08 17:16:20 -0500

    Start mapbox

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // index.js
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZHNjYXJiMjEiLCJhIjoiY2x0cnR3cWlqMGtmZzJucDU2eDR2eWpyMCJ9.nfk8bnbhwkUmEHDhKZv3zA";
+
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  center: [-74.5, 40], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  zoom: 9, // starting zoom
+});
