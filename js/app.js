a1951482176d9bd99105b2e7c06654f000de5620
David Scarborough <dscarb21@gmail.com>
2024-11-08 22:44:26 -0500

    Initial PWA setup and functional

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/hacknhfrontend/sw.js')
-        .then(() => console.log('service worker registered'))
-        .catch(() => console.log('service worker not registered'))
-}
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', async () => {
+      try {
+        let reg;
+  
+        if (import.meta.env?.DEV) {
+          reg = await navigator.serviceWorker.register('/hacknhfrontend/sw.js', {
+            type: 'module',
+          });
+        } else {
+          reg = await navigator.serviceWorker.register('/hacknhfrontend/sw.js');
+        }
+  
+        console.log('Service worker registered! 😎', reg);
+      } catch (err) {
+        console.log('😥 Service worker registration failed: ', err);
+      }
+    });
+  }
