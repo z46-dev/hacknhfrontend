20fe6dfa72d96105545fcc65aa5dd2c5056700fa
z46-dev <evanparker6023@gmail.com>
2024-11-08 22:20:33 -0500

    Fix CORS header through text/plain

--- js/account.js
+++ js/account.js
@@ -3,42 +3,42 @@
  * @param {String} state Current form state (login/register)
  */
 function switchForm(state) {
-  const form = document.getElementById("account");
-  const switchButton = document.getElementById("account-switch");
-  const formHeader = document.getElementById("account-header");
-  const confirmPassword = document.getElementById("confirm");
+    const form = document.getElementById("account");
+    const switchButton = document.getElementById("account-switch");
+    const formHeader = document.getElementById("account-header");
+    const confirmPassword = document.getElementById("confirm");
 
-  // Update form submit
-  form.onsubmit =
-    state === "login"
-      ? function () {
-          submitForm("register");
-        }
-      : function () {
-          submitForm("login");
+    // Update form submit
+    form.onsubmit =
+        state === "login" ?
+        function() {
+            submitForm("register");
+        } :
+        function() {
+            submitForm("login");
         };
 
-  // Update button html
-  switchButton.innerHTML =
-    state === "login"
-      ? "Have an account? Login here"
-      : "Don't have an account? Register now";
+    // Update button html
+    switchButton.innerHTML =
+        state === "login" ?
+        "Have an account? Login here" :
+        "Don't have an account? Register now";
 
-  // Update onclick event
-  switchButton.onclick =
-    state === "login"
-      ? function () {
-          switchForm("register");
-        }
-      : function () {
-          switchForm("login");
+    // Update onclick event
+    switchButton.onclick =
+        state === "login" ?
+        function() {
+            switchForm("register");
+        } :
+        function() {
+            switchForm("login");
         };
 
-  // Change form header
-  formHeader.innerHTML = state === "login" ? "Create an Account:" : "Login:";
+    // Change form header
+    formHeader.innerHTML = state === "login" ? "Create an Account:" : "Login:";
 
-  // Change form header
-  confirmPassword.classList.toggle("hidden");
+    // Change form header
+    confirmPassword.classList.toggle("hidden");
 }
 
 /**
@@ -46,72 +46,75 @@ function switchForm(state) {
  * @param {String} state Current form state (login/register)
  */
 async function formSubmit(state) {
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-  console.log(await createAccount({ username, password }));
+    console.log(await createAccount({
+        username,
+        password
+    }));
 }
 
 async function createAccount(creds) {
-  const res = await fetch("https://localhost:8000/user/create", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(creds),
-  });
+    const res = await fetch("http://127.0.0.1:8000/user/create", {
+        method: "POST",
+        headers: {
+            "Content-Type": "text/plain",
+        },
+        body: JSON.stringify(creds)
+    });
 
-  if (res.status !== 200) {
+    if (res.status !== 200) {
+        return {
+            ok: false,
+            status: res.status,
+        };
+    }
+
     return {
-      ok: false,
-      status: res.status,
+        ok: true,
+        user: await res.json(),
     };
-  }
-
-  return {
-    ok: true,
-    user: await res.json(),
-  };
 }
 
 async function login(creds) {
-  const res = await fetch("http://localhost:8000/user/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(creds()),
-  });
+    const res = await fetch("http://localhost:8000/user/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "text/plain",
+        },
+        body: JSON.stringify(creds()),
+    });
 
-  if (res.status !== 200) {
+    if (res.status !== 200) {
+        return {
+            ok: false,
+            status: res.status,
+        };
+    }
+
     return {
-      ok: false,
-      status: res.status,
+        ok: true,
     };
-  }
-
-  return {
-    ok: true,
-  };
 }
 
 async function getUsers() {
-  const res = await fetch("http://127.0.0.1:8000/user/all", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+    const res = await fetch("http://127.0.0.1:8000/user/all", {
+        method: "GET",
+        headers: {
+            "Content-Type": "text/plain",
+        },
+    });
 
-  if (res.status !== 200) {
+    if (res.status !== 200) {
+        return {
+            ok: false,
+            status: res.status,
+        };
+    }
+
     return {
-      ok: false,
-      status: res.status,
+        ok: true,
+        users: await res.json(),
     };
-  }
-
-  return {
-    ok: true,
-    users: await res.json(),
-  };
 }
